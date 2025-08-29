2ece12489d98493098f866772aea9979b7ac45e1
sofiamellano <mellanosofia1@gamail.com>
2025-08-29 17:33:11 -0300

    Actualizar configuración de servicios en Program.cs
    Se han eliminado líneas redundantes y se ha reorganizado el código para mejorar la claridad en `Program.cs`. Se añadió la configuración de Swagger/OpenAPI y se mantuvo la inyección de dependencias para el `DbContext` utilizando MySQL. También se creó la instancia de la aplicación con `var app = builder.Build();` para configurar el pipeline de solicitudes HTTP.

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,11 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddControllers();
 
 var configuration = new ConfigurationBuilder()
         .AddJsonFile("appsettings.json")
@@ -21,6 +16,10 @@ string cadenaConexion = configuration.GetConnectionString("mysqlRemoto");
 builder.Services.AddDbContext<ParkARContext>(
     options => options.UseMySql(cadenaConexion,
                                 ServerVersion.AutoDetect(cadenaConexion)));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
